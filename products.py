aab235e87cd3ddc37ca24aeee992960e18b02d7e
Bruce Chan <bruce20219@gmail.com>
2020-01-07 21:06:42 +0800

    add loop print

--- products.py
+++ products.py
@@ -6,4 +6,7 @@ while True:
 	strPrice = input("the price is: ")
 	listProduct = [strName,strPrice]
 	listProducts.append([strName,strPrice])
-print ( "Price of '",listProducts[0][0],"' is ", listProducts[0][1])
+#print ( "Price of '",listProducts[0][0],"' is ", listProducts[0][1])
+
+for p in listProducts:
+	print("The price of ", p[0], " is ", p[1])
