312cb27bf0ce28a90471efe547716b4b9c4fbfd5
Bruce Chan <bruce20219@gmail.com>
2020-01-08 21:51:37 +0800

    add utf code

--- products.py
+++ products.py
@@ -4,9 +4,14 @@ while True:
 	if strName == 'q':
 		break
 	strPrice = input("the price is: ")
-	listProduct = [strName,strPrice]
+	listProduct = [strName,int(strPrice)]
 	listProducts.append([strName,strPrice])
 #print ( "Price of '",listProducts[0][0],"' is ", listProducts[0][1])
 
 for p in listProducts:
 	print("The price of ", p[0], " is ", p[1])
+
+with open("Products.csv", "w", encoding = "utf-8") as f:
+	f.write("商品,價格\n")
+	for p in listProducts:
+		f.write(p[0] + "," + str(p[1]) + "\n")
