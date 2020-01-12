7407cc38aecd0b5c8277514f2fc94aac42e61094
Bruce Chan <bruce20219@gmail.com>
2020-01-12 13:58:34 +0800

    Refactor

--- products.py
+++ products.py
@@ -1,35 +1,48 @@
 import os
 
-listProducts = []
-if os.path.isfile("products.csv"): #檢察檔案在不在
-	print("file is exised")
-	# 讀取檔案
-	with open("products.csv", "r", encoding = "utf-8") as f:
+# 讀取檔案
+def read_file(filename):
+	listProducts = []
+	with open(filename, "r", encoding = "utf-8") as f:
 		for line in f:
 			if "商品,價格" in line:
 				continue
 			name, price = line.strip().split(',')
 			listProducts.append([name, price])
-	print(listProducts)
-else:
-	print("file is not existed...")
+	return listProducts
 
-# 讓使用者輸入
-while True:
-	strName = input("Please input product name: ")
-	if strName == 'q':
-		break
-	strPrice = input("the price is: ")
-	listProduct = [strName,int(strPrice)]
-	listProducts.append([strName,strPrice])
-#print ( "Price of '",listProducts[0][0],"' is ", listProducts[0][1])
-
+# 讓使用者輸入''
+def user_input(listProducts):
+	while True:
+		strName = input("Please input product name: ")
+		if strName == 'q':
+			break
+		strPrice = input("the price is: ")
+		listProducts.append([strName,strPrice])
+	return listProducts
+	#print ( "Price of '",listProducts[0][0],"' is ", listProducts[0][1])
 # 進出購買紀錄
-for p in listProducts:
-	print("The price of ", p[0], " is ", p[1])
-
-# 寫入檔案
-with open("Products.csv", "w", encoding = "utf-8") as f:
-	f.write("商品,價格\n")
+def print_products(listProducts):
 	for p in listProducts:
-		f.write(p[0] + "," + str(p[1]) + "\n")
+		print("The price of ", p[0], " is ", p[1])
+# 寫入檔案
+def write_file(filename, listProducts):
+	with open(filename, "w", encoding = "utf-8") as f:
+		f.write("商品,價格\n")
+		for p in listProducts:
+			f.write(p[0] + "," + str(p[1]) + "\n")
+
+
+def main():
+	filename = "Products.csv"
+	listProducts = []
+	if os.path.isfile(filename):
+		print("file is exised")
+		listProducts = read_file(filename)
+	else:
+		print("file is not existed...")
+	listProducts = user_input(listProducts)
+	print_products(listProducts)
+	write_file(filename, listProducts)
+
+main()
